591c61184521f0017b3dd1909a0b069c7e92ae1c
Arif <shaharifabdullah@gmail.com>
2021-10-13 17:51:32 +0600

    init

---
+++ lib/homescreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class homescreen extends StatefulWidget {
+  const homescreen({Key? key}) : super(key: key);
+
+  @override
+  _homescreenState createState() => _homescreenState();
+}
+
+class _homescreenState extends State<homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text("Home"))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:splashscreen/homescreen.dart';
+
+class splashscreen extends StatefulWidget {
+  const splashscreen({Key? key}) : super(key: key);
+
+  @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => homescreen()),
+            (route) => false));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Center(child: Text("Loading....."))],
+      ),
+    );
+  }
+}
